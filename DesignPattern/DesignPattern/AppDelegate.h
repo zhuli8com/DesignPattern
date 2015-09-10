c660238d8d5b9fa969bc688ea77a781426e9e440
zhuli8com <zhuli1228@163.com>
2015-09-10 12:26:36 +0800

    git环境搭建

---
+++ DesignPattern/DesignPattern/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DesignPattern
+//
+//  Created by account on 15/9/10.
+//  Copyright (c) 2015年 zhuli8. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DesignPattern/DesignPattern/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DesignPattern
+//
+//  Created by account on 15/9/10.
+//  Copyright (c) 2015年 zhuli8. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
